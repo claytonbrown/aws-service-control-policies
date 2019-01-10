e8578386711c5b18810a0d7e663fd2347e0a5834
Clayton Brown <dev@claytonbrown.com>
2019-01-10 16:14:28 +1100

    updates

--- deploy_scp.sh
+++ deploy_scp.sh
@@ -3,7 +3,10 @@
 
 for policy_file in Policies/*.json ; do
     policy=`basename $policy_file | sed s/.json//g`
-    echo "Pushing $policy"
+    name=`echo $policy|sed s/Compliance//g | sed s/Regional//g`
+
+    FILESIZE=$(wc -c <"$policy_file")
+    echo "Pushing $policy - $FILESIZE / 5120 bytes "
 
     desc_file="Policies/$policy.txt"
     if [ ! -f $desc_file ] ; then
@@ -13,14 +16,14 @@ for policy_file in Policies/*.json ; do
     description=`cat Policies/$policy.txt`
 
     echo "  ${description}"
-    
-    policy_id=`aws organizations list-policies --filter SERVICE_CONTROL_POLICY | jq --arg p "$policy" -c '.Policies[] | select( .Name | contains($p))' | jq -r .Id `
+
+    policy_id=`aws organizations list-policies --filter SERVICE_CONTROL_POLICY | jq --arg p "$policy" -c '.Policies[] | select( .Name | contains($p))' | jq -r .Id`
     if [ ! -z $policy_id ] ; then
         echo "$policy exists as $policy_id"
-        aws organizations update-policy --policy-id ${policy_id} --content file://$policy_file --name $policy --description "$description" --output text | grep POLICYSUMMARY
+        aws organizations update-policy --policy-id ${policy_id} --content file://$policy_file --name $name --description "$description" --output text | grep POLICYSUMMARY
     else
         echo "$policy doesn't exist yet"
-        aws organizations create-policy --content file://$policy_file --name $policy --description "$description" --type SERVICE_CONTROL_POLICY --output text | grep POLICYSUMMARY
+        aws organizations create-policy --content file://$policy_file --name $name --description "$description" --type SERVICE_CONTROL_POLICY --output text | grep POLICYSUMMARY
     fi
 done
 
